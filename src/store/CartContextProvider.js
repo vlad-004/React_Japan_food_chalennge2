a75ef039864fb2f5bf910a3e166fcbe44f335573
vlad-004 <prostoy-vlad@rambler.ru>
2023-03-13 23:55:50 +0200

    добавление reducer в функционал корзины. Я немного запутался надо будет еще раз разобраться в хуках какой как работает и пересмотреть проект на текущем этапе чтобы понять где и что я упустил в понимании и что меня путает тут.

--- src/store/CartContextProvider.js
+++ src/store/CartContextProvider.js
@@ -1,21 +1,54 @@
 import CartContext from "./cart-context";
+import {useReducer} from "react";
+import cart from "../components/Cart/Cart";
 
-/**Цель этого компонента управлять данными контекста корзины и
+const defaultCartState = {
+    items: [],
+    totalAmount: 0,
+}
+
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD_ITEM') {
+        const updatedItems = state.items.concat(action.item); // при методе concat в переменной будет новый массив со вставленным новым элементом
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount,
+        }
+    }
+    return defaultCartState;
+};
+
+
+/**Цель этого компонента управлять данными корзины (контекста) и
  * предоставлять этот контекст всем компанентам кто имеет к этому
- * контексту доступ. **/
+ * контексту доступ.
+ *
+ * Нам надо будет проверять является ли блюдо частью корзины или это новое блюдо которого нет в корзине.
+ *
+ * Код в этом компоненте будет пересобираться каждый раз при изменении корзины.
+ * **/
 const CartContextProvider = (props) => {
 
-    const addItemHandler = item => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
+    const addItemHandler = (item) => {
+        dispatchCartAction({
+            type: 'ADD_ITEM',
+            item: item,
+        });
     };
 
-    const removeItemHandler = id => {
-
+    const removeItemHandler = (id) => {
+        dispatchCartAction({
+            type: 'REMOVE_ITEM',
+            id: id,
+        });
     };
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemHandler,
         removeItem: removeItemHandler,
     }
